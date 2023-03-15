b4ddf5e59b845cfe3eb9a089a829c7a6e3c40cf4
Murtaza Ali Imtiaz <murtazalimtiaz@gmail.com>
2023-03-15 01:22:05 -0400

    Decrement 11.11111111

--- ca/src/lib.rs
+++ ca/src/lib.rs
@@ -5,5 +5,19 @@ mod s;
 mod tests {
     mod security {
 
+        #[test]
+        fn test_rcgen() {
+            extern crate rcgen;
+            use rcgen::generate_simple_self_signed;
+            let subject_alt_names =
+                vec!["hello.world.happy".to_string(), "localhost".to_string()];
+
+            let cert = generate_simple_self_signed(subject_alt_names).unwrap();
+            // The certificate is now valid for localhost and the domain "hello.world.example"
+            println!("{}", cert.serialize_pem().unwrap());
+            println!("{}", cert.serialize_private_key_pem());
+            
+            assert!(true);
+        }
     }
 }
